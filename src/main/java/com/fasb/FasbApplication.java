72c47915a126e8ea2baa82f777a19801be276ba4
dragan <dragan.velkovski@codeit.mk>
2020-02-22 11:00:48 +0100

    Initial commit with project generated with Spring Initializr

---
+++ src/main/java/com/fasb/FasbApplication.java
@@ -0,0 +1,13 @@
+package com.fasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasbApplication.class, args);
+	}
+
+}
