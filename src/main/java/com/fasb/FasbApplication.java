a9dd1c965009eff382354efcaea8e95c2f2b1feb
Dragan Velkovski <draganvelkovski77@gmail.com>
2020-03-29 22:21:55 +0200

    cors2

--- src/main/java/com/fasb/FasbApplication.java
+++ src/main/java/com/fasb/FasbApplication.java
@@ -35,7 +35,7 @@ public class FasbApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("http://localhost:4200","http://localhost:63342");
+				registry.addMapping("/**");
 			}
 		};
 	}
